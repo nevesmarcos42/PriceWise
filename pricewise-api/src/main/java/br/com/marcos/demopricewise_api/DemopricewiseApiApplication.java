3a8f1cafb86f66947136fd0189bda359acdeed80
nevesmarcos42 <nevesmarcos42@gmail.com>
2025-06-21 17:33:36 -0300

    Primeiro commit adiciona código base gerado pelo Spring Initializr com dependências do projeto

---
+++ pricewise-api/src/main/java/br/com/marcos/demopricewise_api/DemopricewiseApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.marcos.demopricewise_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemopricewiseApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemopricewiseApiApplication.class, args);
+	}
+
+}
---
+++ pricewise-api/src/test/java/br/com/marcos/demopricewise_api/DemopricewiseApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.marcos.demopricewise_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemopricewiseApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
